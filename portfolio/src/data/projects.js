75249e1073dafea1e9acb346c7a446471cd00ff0
Nevin Herring <nevin.herring@gmail.com>
2019-03-31 17:16:50 -0500

    added projects

---
+++ portfolio/src/data/projects.js
@@ -0,0 +1,21 @@
+import project1 from '../assets/storage.png';
+import project2 from '../assets/miners.png';
+
+const PROJECTS = [
+    {
+        id: 1,
+        title: '157 Storage Homepage',
+        description: 'A static site for 157 Storage, a local business',
+        link: 'http://157storage.com',
+        image: project1
+    },
+    {
+        id: 2,
+        title: 'Miners Theatre Homepage',
+        description: 'A static site for Miners Theatre Institute Foundation',
+        link: 'http://minerstheatre.com',
+        image: project2
+    }
+]
+
+export default PROJECTS;
